87e3985eed540d104a4eb7327f8cb30db0eb92c5
Anna <annanen@inbox.ru>
2023-11-05 20:57:47 +0300

    Tasks 1 - 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,24 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+//TASK_1
+        System.out.println("Задача № 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println("______________________________");
 
+//TASK_2
+        System.out.println("Задача № 2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("______________________________");
+
+//TASK_3
+        System.out.println("Задача № 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
         }
     }
